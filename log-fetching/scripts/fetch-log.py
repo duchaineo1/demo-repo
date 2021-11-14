74822fbbba348402faa5aa297ef818d23959c621
Olivier Duchaine <oduchaine@coveo.com>
2021-11-13 22:21:36 -0500

    function to gather log file names

--- log-fetching/scripts/fetch-log.py
+++ log-fetching/scripts/fetch-log.py
@@ -2,7 +2,19 @@ import os
 from re import match
 
 
-def fetch_logs(file_name):
+BASE_DIR = "/var/log/apache2/"
+
+
+def get_access_log_files(base_dir: str) -> list:
+    files = os.listdir(base_dir)
+    access_log = []
+    for log_file in files:
+        if "access" in log_file and "gz" not in log_file:
+            access_log.append(log_file)
+    return access_log
+
+
+def fetch_logs(file_name: str) -> dict:
     ips = {}
     pattern = "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
     for log_file in file_name:
@@ -11,17 +23,15 @@ def fetch_logs(file_name):
             ip = line.split(" ")[0]
             if match(pattern, ip):
                 ips[ip] = ips.get(ip, 0) + 1
-
     return ips
 
 
-def calculate_total_hits(ip_hits_dictionnary):
+def calculate_total_hits(ip_hits_dictionnary: dict):
     total = 0
     for ip in ip_hits_dictionnary:
+        print(ip, ip_hits_dictionnary[ip])
         total += ip_hits_dictionnary[ip]
     return f"Total hits : {total}"
 
 
-calculate_total_hits(
-    fetch_logs(["/var/log/apache2/access.log", "/var/log/apache2/access.log.1"])
-)
+fetch_logs(get_access_log_files())
