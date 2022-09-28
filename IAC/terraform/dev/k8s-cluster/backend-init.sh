76de5c94c6e241f60123757b5eea35523ac6f4d8
oduchaine <oduchaine@protonmail.com>
2022-09-28 10:15:34 -0400

    add error handling for missing api token export

--- IAC/terraform/dev/k8s-cluster/backend-init.sh
+++ IAC/terraform/dev/k8s-cluster/backend-init.sh
@@ -9,6 +9,13 @@ export TF_VAR_region="us-east-1"
 export TF_VAR_environment="dev"
 export TF_VAR_state_file_name="terraform.tfstate"
 
+if [ -z ${SPACES_SECRET_KEY+x} ] || [ -z ${SPACES_ACCESS_TOKEN+x} ]; then
+    echo "You need to export both your space API tokens and secret in order to initialize your state";
+    echo "Example: export SPACES_ACCESS_TOKEN='mytoken' && export SPACES_SECRET_KEY='mysecretkey'"
+    echo "See: https://www.digitalocean.com/community/tutorials/how-to-create-a-digitalocean-space-and-api-key"
+    exit 1;
+fi
+
 if [ -z ${RECONFIGURE+x} ]; then
     terraform init \
          -backend-config "bucket=$TF_VAR_tf_state_bucket" \
