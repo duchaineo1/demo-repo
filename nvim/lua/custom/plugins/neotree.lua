fc4324ad4e35a12ec05f7c39f3c30d5a4f8b8920
olivier-duchaine <oduchaine@simons.ca>
2024-11-08 19:51:38 -0500

    remove old nvim

---
+++ nvim/lua/custom/plugins/neotree.lua
@@ -0,0 +1,61 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    'echasnovski/mini.icons',
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  opts = {
+    default_component_configs = {
+      icon = {
+        provider = function(icon, node) -- setup a custom icon provider
+          local text, hl
+          local mini_icons = require 'mini.icons'
+          if node.type == 'file' then -- if it's a file, set the text/hl
+            text, hl = mini_icons.get('file', node.name)
+          elseif node.type == 'directory' then -- get directory icons
+            text, hl = mini_icons.get('directory', node.name)
+            -- only set the icon text if it is not expanded
+            if node:is_expanded() then
+              text = nil
+            end
+          end
+
+          -- set the icon text/highlight only if it exists
+          if text then
+            icon.text = text
+          end
+          if hl then
+            icon.highlight = hl
+          end
+        end,
+      },
+      kind_icon = {
+        provider = function(icon, node)
+          local mini_icons = require 'mini.icons'
+          icon.text, icon.highlight = mini_icons.get('lsp', node.extra.kind.name)
+        end,
+      },
+    },
+    window = {
+      position = 'right',
+      mappings = {
+        ['Y'] = 'none',
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_by_name = {
+          '.DS_Store',
+        },
+        always_show = {
+          '.env',
+        },
+      },
+    },
+  },
+}
